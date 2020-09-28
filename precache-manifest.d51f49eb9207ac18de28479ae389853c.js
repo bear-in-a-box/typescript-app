b9243116e26e802cc4930e0acfa02d4a022b320d
Konrad <KingLoczek@users.noreply.github.com>
2020-09-28 13:32:55 +0200

    Updates

---
+++ precache-manifest.d51f49eb9207ac18de28479ae389853c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "487be83318a0e67f88e4cb44d719f7b0",
+    "url": "/typescript-app/index.html"
+  },
+  {
+    "revision": "7b43f19b3f811a5b7186",
+    "url": "/typescript-app/static/css/main.d90debc8.chunk.css"
+  },
+  {
+    "revision": "096755a1adf7e134b83d",
+    "url": "/typescript-app/static/js/2.d8fa18da.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/typescript-app/static/js/2.d8fa18da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b43f19b3f811a5b7186",
+    "url": "/typescript-app/static/js/main.cc18faa0.chunk.js"
+  },
+  {
+    "revision": "0a2e2211c2caa0575b7c",
+    "url": "/typescript-app/static/js/runtime-main.b6ed7324.js"
+  }
+]);
